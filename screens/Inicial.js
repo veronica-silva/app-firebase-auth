a12fd0942318edc08c9912c7165c577f3cc48f7a
Veronica Silva <78879969+veronica-silva@users.noreply.github.com>
2023-02-02 11:01:53 -0300

    alterações para atualização

--- screens/Inicial.js
+++ screens/Inicial.js
@@ -1,50 +1,57 @@
-import { Button, StyleSheet, Text, View } from 'react-native'
+import { Button, StyleSheet, Text, View } from "react-native";
 
 const Inicial = ({ navigation }) => {
-    return (
-        <View style={estilos.container}>
-            <View style={estilos.introducao}>
-                <Text style={estilos.titulo}>React Native</Text>
-                <Text style={estilos.subtitulo}>Autenticação com Firebase</Text>
-            </View>
-            <View style={estilos.botoes}>
-                <Button title='Entre' color="green" onPress={() => navigation.navigate("Login")} />
-                <Button title='Cadastre-se' color="blue" onPress={() => navigation.navigate("Cadastro")} />
-            </View>
+  return (
+    <View style={estilos.container}>
+      <View style={estilos.introducao}>
+        <Text style={estilos.titulo}>App Auth</Text>
+        <Text style={estilos.subtitulo}>Autenticação com Firebase</Text>
+      </View>
+      <View style={estilos.botoes}>
+        <Button
+          title="Entre Agora"
+          color="black"
+          onPress={() => navigation.navigate("Login")}
+        />
+        <Button
+          title="Cadastre-se"
+          color="red"
+          onPress={() => navigation.navigate("Cadastro")}
+        />
+      </View>
+    </View>
+  );
+};
 
-        </View>
-    )
-}
-
-export default Inicial
+export default Inicial;
 
 const estilos = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-        padding: 16
-    },
-    introducao: {
-        backgroundColor: "#4A235A",
-        width: "100%",
-        padding: 8,
-        borderRadius: 4
-    },
-    titulo: {
-        textAlign: "center",
-        fontSize: 32,
-        color: "white",
-    },
-    subtitulo: {
-        textAlign: "center",
-        color: "white"
-    },
-    botoes: {
-        marginVertical: 8,
-        width: "100%",
-        flexDirection: "row",
-        justifyContent: "space-evenly"
-    }
-})
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 16,
+  },
+  introducao: {
+    backgroundColor: "#4A235A",
+    width: "100%",
+    padding: 8,
+    borderRadius: 4,
+  },
+  titulo: {
+    textAlign: "center",
+    fontSize: 32,
+    color: "white",
+  },
+  subtitulo: {
+    textAlign: "center",
+    color: "white",
+  },
+  botoes: {
+    marginVertical: 8,
+    width: "100%",
+    flexDirection: "row",
+    justifyContent: "space-evenly",
+  },
+});
